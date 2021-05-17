077697fe1fa17107bd92cf95021547fd2172a7cc
pholawat-tangsatit <pholawat.tangsatit@hotmail.com>
2021-05-17 13:36:07 +0700

    implement updateRecord in cron job

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const publicIp = require('public-ip');
+const cron = require('node-cron');
 
-var cron = require('node-cron');
+require('dotenv').config();
 
-let currentIP = '';
+const domainName = process.env.domainName;
+const namedotcom_username = process.env.namedotcom_username;
+const namedotcom_apiToken = process.env.namedotcom_apiToken;
+const Domain = require('./functions/domain');
+
+const pholawat_codes = new Domain(
+    domainName,
+    namedotcom_username,
+    namedotcom_apiToken
+);
+
+let currentIP;
 
 cron.schedule('*/10 * * * * *', async () => {
     let newIP = await publicIp.v4();
     if (currentIP != newIP) {
         currentIP = newIP;
-        console.log(`Changed IP to ${currentIP}`);
+        await pholawat_codes.updateRecord('pholawat.codes', currentIP);
     }
 });
